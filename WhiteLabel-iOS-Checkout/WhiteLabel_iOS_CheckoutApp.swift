c0b14960d6098f6dc1e9dc67962c9606a8298ea0
Talisson Bento <talisson.bento@zoop.com.br>
2021-03-08 18:22:08 -0300

    initial commit

---
+++ WhiteLabel-iOS-Checkout/WhiteLabel_iOS_CheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteLabel_iOS_CheckoutApp.swift
+//  WhiteLabel-iOS-Checkout
+//
+//  Created by Talisson Bento | Zoop on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WhiteLabel_iOS_CheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
